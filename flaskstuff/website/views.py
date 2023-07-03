f55bec788d9e48cd4d435460efb0d414d29ca623
lukewarmspaghettisauce <amli1@ualberta.ca>
2023-07-03 13:07:57 -0600

    can edit orders/packages from website now

--- flaskstuff/website/views.py
+++ flaskstuff/website/views.py
@@ -6,9 +6,11 @@ from .models import Note
 from . import db
 import json
 
-#from backend.shipping_objects import *
-from backend.backend import *
+from backend.shipping_objects import *
+saver = Save_Data(filename="pickle_data.json")
+shipment = saver.load_data(restart_data=True)
 
+print('---------RESTARTING VIEWS')
 # blueprint defines a bunch of URLs
 # naming it 'views' is optional but simplifies things
 views = Blueprint('views', __name__)
@@ -67,26 +69,41 @@ def pk_details(pk_id):
     package = shipment.package(pk_id)
     if request.method == 'POST':
         form_data = request.form
+            # ASSUMES THAT SHIPPER == CUSTOMER
         if form_data['save_btn'] == 'shipper-info':
-            package.shipper.name = form_data['shipper_name']
-            package.shipper.email = form_data['shipper_email']
-            package.shipper.phone = form_data['shipper_phone']
-            package.shipper.address = form_data['shipper_address']
+            package.customer.name = form_data['shipper_name']
+            package.customer.email = form_data['shipper_email']
+            package.customer.phone = form_data['shipper_phone']
+            package.customer.address = form_data['shipper_address']
+            package.customer.city = form_data['shipper_city']
+            package.customer.state = form_data['shipper_state']
+            package.customer.zip_code = form_data['shipper_zip']
         elif form_data['save_btn'] == 'consignee-info':
             package.consignee.name = form_data['consignee_name']
             package.consignee.email = form_data['consignee_email']
             package.consignee.phone = form_data['consignee_phone']
             package.consignee.address = form_data['consignee_address']
+            package.consignee.city = form_data['consignee_city']
+            package.consignee.state = form_data['consignee_state']
+            package.consignee.zip_code = form_data['consignee_zip']
         elif form_data['save_btn'] == 'additional-info':
-            package.has_batteries = True if 'has_batteries' in form_data else False
-            package.insurance = True if 'wants_insurance' in form_data else False
+            package.has_batteries = 'lithium_batteries' in form_data
+            package.fragile = 'is_fragile' in form_data
+            package.set_dimensions(
+                length=float(form_data['length']),
+                width=float(form_data['width']),
+                height=float(form_data['height'])
+            )
+            package.dim_units = form_data['units']
+            package.weight = float(form_data['weight'])
+            package.description = form_data['package_description']
         else:
             raise ValueError("Do not recognize save button value, check pk_details.html")
 
     return render_template('pk_details.html', pk=package)
 # endregion
 
-# region view/edit package details
+# region add new order
 @views.route('/add-order', methods=['GET', 'POST'])
 def add_order():
     global shipment
@@ -102,24 +119,30 @@ def add_order():
         # fragile option doesn't do anything; need to add to shipping_objects
         if action == 'add':
             # assumes shipper = customer
-            name = form_data['shipper-name']
-            address = form_data['shipper-address']
-            city = form_data['shipper-city']
-            state = form_data['shipper-state']
-            zipcode = form_data['shipper-zip']
-            phone = form_data['shipper-phone']
-            email = form_data['shipper-email']
-            customer = Customer(name, address, city, state, zipcode, phone, email)
-            shipper = Shipper(name, address, city, state, zipcode, phone, email)
-
-            name = form_data['consignee-name']
-            address = form_data['consignee-address']
-            city = form_data['consignee-city']
-            state = form_data['consignee-state']
-            zipcode = form_data['consignee-zip']
-            phone = form_data['consignee-phone']
-            email = form_data['consignee-email']
-            consignee = Consignee(name, address, city, state, zipcode, phone, email)
+            customer = Customer(
+                name=form_data['shipper-name'], 
+                address=form_data['shipper-address'], 
+                city=form_data['shipper-city'], 
+                state=form_data['shipper-state'], 
+                zip_code=form_data['shipper-zip'], 
+                phone=form_data['shipper-phone'], 
+                email=form_data['shipper-email'])
+            shipper = Shipper(
+                name=form_data['shipper-name'], 
+                address=form_data['shipper-address'], 
+                city=form_data['shipper-city'], 
+                state=form_data['shipper-state'], 
+                zip_code=form_data['shipper-zip'], 
+                phone=form_data['shipper-phone'], 
+                email=form_data['shipper-email'])
+            consignee = Consignee(
+                name=form_data['consignee-name'], 
+                address=form_data['consignee-address'], 
+                city=form_data['consignee-city'], 
+                state=form_data['consignee-state'], 
+                zip_code=form_data['consignee-zip'], # returns blank string if nothing there
+                phone=form_data['consignee-phone'], 
+                email=form_data['consignee-email'])
             
             pickup_address = None
             if form_data.get('office-drop-off') == 'drop-off':
@@ -128,34 +151,49 @@ def add_order():
                 office_dropoff = False
                 pickup_address = form_data.get('pickup-address')
             # delivery address is consignee address by default
-            office_pickup = True if form_data.get('office-pickup') == 'pick-up' else False
-            insurance = True if form_data.get('insurance') == 'on' else False
+            office_pickup = form_data.get('office-pickup') == 'pick-up'
+            insurance = form_data.get('insurance') == 'on' 
             order = CustomerOrder(customer, "", office_dropoff, office_pickup, insurance)
             order.assign_shipment(shipment)
 
             # region BOXES
-            box_num = form_data.get('number-of-boxes')
+            box_num = form_data.get('box-count')
             if box_num is None or int(box_num) == 0:
                 error_message = "Please enter a valid number of boxes (greater than 0)."
+                print("-------------------Please enter a valid number of boxes (greater than 0).")
+                # FIXME error message does not work
+                # do error checking from within the template instead of here
+                # caused an error where site was redirecting to add order page upon submit
+                # without knowing why
                 return render_template('add_order.html', error=error_message)
             
             box_num = int(box_num)
             for i in range(1, box_num+1):
-                dim = (form_data.get(f'length-{i}'),
-                       form_data.get(f'width-{i}'),
-                       form_data.get(f'height-{i}'))
+                dim = (float(form_data.get(f'length-{i}')),
+                       float(form_data.get(f'width-{i}')),
+                       float(form_data.get(f'height-{i}')))
                 units = form_data.get(f'units-{i}')
-                weight = int(form_data[f'weight-{i}'])
+                weight = float(form_data[f'weight-{i}'])
                 desc = form_data[f'box-cargo-description-{i}']
-                batteries = True if form_data.get(f'box-lithium-batteries-{i}') == 'on' else False
-                fragile = True if form_data.get(f'box-fragile-{i}') == 'on' else False  # does nothing
+                batteries = form_data.get(f'box-lithium-batteries-{i}') == 'on' 
+                fragile = form_data.get(f'box-fragile-{i}') == 'on'
                 pk = Package(dim, units, weight, order, shipper, consignee, desc, batteries)
-                order.add_package(pk)
+                print(f"----------UNITS {units}")
             # endregion
-            print(shipment) #FIXME not working
+            """
+            {% if not data %}
+                {% set data = {'boxes':{}} %}
+            {% endif %}
+            """
+            print('-----------------SHIPMENT')
+            print(shipment.package_array) #FIXME not working
             print(order)
             flash('Order added', category='success')
-            return render_template('add_order.html')
+            autofill_dict = {'boxes':{}}
+            
+            saver.save_data(shipment)
+            #return render_template('add_order.html', data=autofill_dict)
+            return redirect(url_for('views.home'))
         elif action == 'cancel':
             return redirect(url_for('views.home'))
         
@@ -180,7 +218,6 @@ def add_order():
         'box_num': 1
     }
 
-    #for i in range(1, autofill_dict['box_num']):
     box = {
         'length': 2,
         'width': 10,
@@ -191,6 +228,7 @@ def add_order():
         'batteries': True,
         'fragile': False,
     }
+
     autofill_dict['boxes'] = box
     
     # region chunk
@@ -212,10 +250,10 @@ def add_order():
         raise ValueError("Do not recognize save button value, check pk_details.html")
     """
     # endregion
-
     return render_template('add_order.html', data=autofill_dict)
 # endregion
 
 #TODO:
-# use text files to store data
+# redesign package editing page
+# finish package removal (shipping_objects.py)
 # finish add order and add package routes
