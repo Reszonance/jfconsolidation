cd691a2f943734692c102736474770872b27bc52
lukewarmspaghettisauce <amli1@ualberta.ca>
2023-08-22 19:19:05 -0600

    excluding google forms file

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = create_app()
 # any file that imports website will do app.run()
 # debug=True will update web server when code changes
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 10000)))#, debug=True
     
 else:
     gunicorn_app = create_app()
